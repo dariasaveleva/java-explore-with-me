0876374e567d62f0139acd55ffca6762d834a990
Dariasaveleva <daria_saveleva@mail.ru>
2023-03-28 22:01:50 +0300

    third check explore-with-me

---
+++ ewn-stat/stat-dto/src/main/java/ru/practicum/ewm/dto/EndpointHitDto.java
@@ -0,0 +1,30 @@
+package ru.practicum.ewm.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class EndpointHitDto {
+    Long id;
+    @NotNull
+    @NotBlank
+    String app;
+    @NotNull
+    @NotBlank
+    String uri;
+    @NotBlank
+    String ip;
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime timestamp;
+}
---
+++ ewn-stat/stat-server/src/main/java/ru/practicum/stat/service/StatServiceImpl.java
@@ -0,0 +1,50 @@
+package ru.practicum.stat.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.ewm.dto.EndpointHitDto;
+import ru.practicum.ewm.dto.ViewStatDto;
+import ru.practicum.stat.mapper.EndpointHitMapper;
+import ru.practicum.stat.mapper.StatMapper;
+import ru.practicum.stat.model.EndpointHit;
+import ru.practicum.stat.repository.StatRepository;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class StatServiceImpl implements StatService {
+    private final StatRepository repository;
+
+
+    @Override
+    public List<ViewStatDto> getStat(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        log.info("Статистика найдена");
+        if (uris.isEmpty() || uris == null) return Collections.emptyList();
+        if (unique) {
+             return repository.getUniqueStat(start, end, uris).stream()
+                     .map(StatMapper::toStatDto)
+                     .collect(Collectors.toList());
+        } else {
+            return repository.getNotUniqueStat(start, end, uris).stream()
+                    .map(StatMapper::toStatDto)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Override
+    @Transactional
+    public EndpointHitDto create(EndpointHitDto endpointHitDto) {
+        EndpointHit endpointHit = repository.save(EndpointHitMapper.toEndpointHit(endpointHitDto));
+        log.info("Запрос сохранён с id {}", endpointHit.getId());
+        return EndpointHitMapper.toEndpointHitDto(endpointHit);
+    }
+}
