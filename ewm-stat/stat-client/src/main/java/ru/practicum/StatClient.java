a14af5916bb7b6fb611f2ec6c4962b04f36fe1d6
Dariasaveleva <daria_saveleva@mail.ru>
2023-03-29 09:13:43 +0300

    changed port

--- ewm-stat/stat-client/src/main/java/ru/practicum/StatClient.java
+++ ewm-stat/stat-client/src/main/java/ru/practicum/StatClient.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import ru.practicum.ewm.dto.EndpointHitDto;
 
 import java.util.List;
@@ -18,26 +19,29 @@ public class StatClient {
     private final WebClient webClient;
 
     public ResponseEntity<Object> post(EndpointHitDto endpointHitDto) {
-        try {
-             webClient.post()
+
+        return webClient.post()
                     .uri("/hit")
                     .contentType(MediaType.APPLICATION_JSON)
                     .body(BodyInserters.fromValue(endpointHitDto))
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .block();
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+                    .exchangeToMono(clientResponse -> {
+                    if (clientResponse.statusCode().equals(HttpStatus.CREATED)) {
+                        return clientResponse.bodyToMono(Object.class)
+                                .map(body -> ResponseEntity.status(HttpStatus.CREATED).body(body));
+                } else {
+                        return clientResponse.createException()
+                                .flatMap(Mono::error);
+                    }
+
+    })
+                .block();
     }
 
     public ResponseEntity<Object> get(String start, String end,
                                        List<String> uris, Boolean unique) {
 
         String params = uris.stream().reduce("", (result, uri) -> result + "&uris=" + uri);
-        try {
-            webClient.get()
+        return webClient.get()
                     .uri(uriBuilder -> uriBuilder
                             .path("/stats")
                             .queryParam("start", start)
@@ -45,14 +49,15 @@ public class StatClient {
                             .query(params)
                             .queryParam("unique", unique)
                             .build())
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .block();
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-
+                .exchangeToMono(clientResponse -> {
+                    if(clientResponse.statusCode().is2xxSuccessful()) {
+                        return clientResponse.bodyToMono(Object.class)
+                                .map(body -> ResponseEntity.ok().body(body));
+                    } else {
+                        return clientResponse.createException().flatMap(Mono::error);
+                    }
+                })
+                .block();
     }
 
 }
