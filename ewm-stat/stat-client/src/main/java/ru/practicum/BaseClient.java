10e1c0530dcbc932bd63186db38cb32b0e4fbb15
Dariasaveleva <daria_saveleva@mail.ru>
2023-03-29 09:13:43 +0300

    changed port

--- ewm-stat/stat-client/src/main/java/ru/practicum/BaseClient.java
+++ ewm-stat/stat-client/src/main/java/ru/practicum/BaseClient.java
@@ -5,8 +5,6 @@ import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
@@ -30,7 +28,6 @@ public class BaseClient {
         return WebClient.builder()
                 .baseUrl(URL)
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 }
